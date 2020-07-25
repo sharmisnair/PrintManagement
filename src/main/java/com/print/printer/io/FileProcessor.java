ddd298b40a3a6ac73421c53e9933d9afe32ab21f
Sharmi Nair <sharmisnair@gmail.com>
2020-07-25 20:19:57 +1000

    Add UML diagram including core classes only for README

--- src/main/java/com/print/printer/io/FileProcessor.java
+++ src/main/java/com/print/printer/io/FileProcessor.java
@@ -1,10 +1,8 @@
 package com.print.printer.io;
 
 import com.print.printer.PrintJob;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.List;
 
 public interface FileProcessor {
-  public List<PrintJob> processInputFile(String inputFilePath) throws FileNotFoundException, IOException, Exception;
+  List<PrintJob> processInputFile(String inputFilePath) throws Exception;
 }
